441020a4aaca4623f1606bfa77e14e4b371f0e6b
BogdanKrik <117049510+BogdanKrik@users.noreply.github.com>
2023-04-25 16:52:31 +0300

    Update change_pass_page.py

--- .pages/change_pass_page.py
+++ .pages/change_pass_page.py
@@ -1,74 +1,75 @@
-Из импорта настроек valid_phone, valid_email, sql_injection 
-от . base_page импорт BasePage
-от . локаторы импортируют AuthPageLocators, ChangePassPageLocators, UserAgreementPageLocators, \
+from settings import valid_phone, valid_email, sql_injection
+from .base_page import BasePage
+from .locators import AuthPageLocators, ChangePassPageLocators, UserAgreementPageLocators, \
     RejectedRequestPageLocators, BaseLocators
 
 
-класс ChangePassPage(BasePage):
+class ChangePassPage(BasePage):
     # TRT009 метод проверки типа восстановления пароля по умолчанию
-    def default_password_recovery_type(self):
-        Самоутвердиться. is_element_present(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT_PLACEHOLDER_TELEPHONE), \
-            "Элемент не найден"
+   def default_password_recovery_type(self):
+        assert self.is_element_present(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT_PLACEHOLDER_TELEPHONE), \
+            "element not found"
 
     # TRT010 метод проверки на валидацию поля ввода номера телефона /почты /логина /лицевого счета (ввод валидного номера)
     def phone_field_validation_valid_data(self):
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_TAB_PHONE). щелчок()
-        телефон = valid_phone()
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT). send_keys(телефон)
-        себя. find_element(BaseLocators. ТЕЛО). щелчок()
-        элемент = самость. find_element(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT_VALUE)
-        value = элемент. get_attribute("значение")
-        assert ("7"+str(phone)) == значение, "phone не совпадает"
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_TAB_PHONE).click()
+        phone = valid_phone()
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT).send_keys(phone)
+        self.find_element(BaseLocators.BODY).click()
+        element = self.find_element(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT_VALUE)
+        value = element.get_attribute("value")
+        assert ("7"+str(phone)) == value, "phone do not match"
 
     # TRT011 метод проверки на валидацию поля ввода номера телефона /почты /логина /лицевого счета (ввод валидного email)
     def email_field_validation_valid_data(self):
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_TAB_MAIL). щелчок()
-        username_input = себя. find_element(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT)
-        электронная почта = valid_email()
-        username_input. send_keys(электронная почта)
-        себя. find_element(BaseLocators. ТЕЛО). щелчок()
-        элемент = самость. find_element(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT_VALUE)
-        value = элемент. get_attribute("значение")
-        assert email == значение, "email не совпадает" 
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_TAB_MAIL).click()
+        username_input = self.find_element(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT)
+        email = valid_email()
+        username_input.send_keys(email)
+        self.find_element(BaseLocators.BODY).click()
+        element = self.find_element(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT_VALUE)
+        value = element.get_attribute("value")
+        assert email == value, "email do not match"
 
     # TRT012 метод проверки кнопки на форму авторизации
     def go_back_button(self):
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_GO_BACK_BUTTON). щелчок()
-        Самоутвердиться. is_element_present(AuthPageLocators. AUTH_HEADING), "элемент не найден"
-        утверждать «https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/authenticate» в себе. браузер. current_url, \
-            «URL-адрес не совпадает»
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_GO_BACK_BUTTON).click()
+        assert self.is_element_present(AuthPageLocators.AUTH_HEADING), "element not found"
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/authenticate" in self.browser.current_url, \
+            "url do not match"
 
     # TRT013 метод проверки ссылки в футере на страницу с пользовательским соглашением
-    def link_to_the_user_agreement_page(self):
-        original_window = себя. браузер. current_window_handle
-        Assert len(self. браузер. window_handles) == 1
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_PRIVACY_POLICY_LINK). щелчок()
-        для window_handle в себе. браузер. window_handles:
-            Если window_handle != original_window:
-                себя. браузер. switch_to. окно(window_handle)
-            Еще:
-                проходить
-        Самоутвердиться. is_element_present(UserAgreementPageLocators. USER_AGREEMENT_HEADING), "элемент не найден"
-        утверждать «https://b2c.passport.rt.ru/sso-static/agreement/agreement.html» в себе. браузер. current_url, \
-            «URL-адрес не совпадает»
+   def link_to_the_user_agreement_page(self):
+        original_window = self.browser.current_window_handle
+        assert len(self.browser.window_handles) == 1
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_PRIVACY_POLICY_LINK).click()
+        for window_handle in self.browser.window_handles:
+            if window_handle != original_window:
+                self.browser.switch_to.window(window_handle)
+            else:
+                pass
+        assert self.is_element_present(UserAgreementPageLocators.USER_AGREEMENT_HEADING), "element not found"
+        assert "https://b2c.passport.rt.ru/sso-static/agreement/agreement.html" in self.browser.current_url, \
+            "url do not match"
+
 
     # TRT014 метод проверки восстановления пароля с незаполненными полями
     def password_recovery_with_blank_fields(self):
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_TAB_PHONE). щелчок()
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_CONTINUE_BUTTON). щелчок()
-        Утверждение "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
-               в себе. браузер. current_url, "URL не совпадает"
-        Самоутвердиться. is_element_present(ChangePassPageLocators. CHANGE_PASS_ERROR_ENTER_PHONE_NUMBER), \
-            "Элемент не найден"
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_TAB_PHONE).click()
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_CONTINUE_BUTTON).click()
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
+               in self.browser.current_url, "url do not match"
+        assert self.is_element_present(ChangePassPageLocators.CHANGE_PASS_ERROR_ENTER_PHONE_NUMBER), \
+            "element not found"
 
     # TRT015 метод проверки восстановления пароля с незаполненным значением капчи
-    def password_recovery_with_blank_captcha(self):
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_TAB_MAIL). щелчок()
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_USERNAME_INPUT). send_keys(valid_email())
-        себя. find_element(ChangePassPageLocators. CHANGE_PASS_CONTINUE_BUTTON). щелчок()
-        Утверждение "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
-               в себе. браузер. current_url, "URL не совпадает"
-        Самоутвердиться. is_element_present(ChangePassPageLocators. CHANGE_PASS_ERROR_INVALID_USERNAME_OR_TEXT), \
-            "Элемент не найден"
+     def password_recovery_with_blank_captcha(self):
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_TAB_MAIL).click()
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_USERNAME_INPUT).send_keys(valid_email())
+        self.find_element(ChangePassPageLocators.CHANGE_PASS_CONTINUE_BUTTON).click()
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
+               in self.browser.current_url, "url do not match"
+        assert self.is_element_present(ChangePassPageLocators.CHANGE_PASS_ERROR_INVALID_USERNAME_OR_TEXT), \
+            "element not found"
 
     
