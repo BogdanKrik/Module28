0f33458eb3a9a34a69e7ddf423c718881e16f783
BogdanKrik <117049510+BogdanKrik@users.noreply.github.com>
2023-04-25 16:39:46 +0300

    Update reg_page.py

--- .pages/reg_page.py
+++ .pages/reg_page.py
@@ -3,19 +3,19 @@
 
 
 класс RegPage(BasePage):
-    # RT021 метод проверки расположения полей ввода, кнопки "Зарегистрироваться", ссылки на пользовательское соглашение
+    # TRT016 метод проверки расположения полей ввода, кнопки "Зарегистрироваться", ссылки на пользовательское соглашение
     def location_of_input_fields_and_buttons_and_links(self):
         Самоутвердиться. is_element_present(RegPageLocators. REG_FIRST_NAME_INPUT_PAGE_RIGHT), "элемент не найден"
         Самоутвердиться. is_element_present(RegPageLocators. REG_REGISTER_BUTTON_PAGE_RIGHT), "элемент не найден"
         Самоутвердиться. is_element_present(RegPageLocators. REG_USER_AGREEMENT_LINK_PAGE_RIGHT), "элемент не найден"
 
-    # RT022 метод проверки валидации текстового поля (ввод валидных данных)
+    # RT017 метод проверки валидации текстового поля (ввод валидных данных)
     def text_field_validation_valid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_FIRST_NAME_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
         Самоутвердиться. is_not_element_present(RegPageLocators. REG_ERROR_FIRST_NAME_INPUT), "найденный элемент"
 
-    # RT023 метод проверки на валидацию поля ввода email или мобильного телефона (ввод валидных данных)
+    # TRT018 метод проверки на валидацию поля ввода email или мобильного телефона (ввод валидных данных)
     def email_or_phone_field_validation_valid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_EMAIL_PHONE_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
@@ -24,13 +24,13 @@
         Assert  input_text == значение, "электронная почта или телефон не совпадают"
         Самоутвердиться. is_not_element_present(RegPageLocators. REG_ERROR_INVALID_EMAIL_OR_PHONE_INPUT), "найденный элемент"
 
-    # RT024 метод проверки на валидацию поля ввода пароля (ввод валидных данных)
+    # RT019 метод проверки на валидацию поля ввода пароля (ввод валидных данных)
     def password_field_validation_valid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_PASSWORD_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
         Самоутвердиться. is_not_element_present(RegPageLocators. REG_ERROR_INVALID_PASSWORD_INPUT), "элемент найден"
 
-    # RT025 метод проверки регистрации с валидными данными
+    # TRT020 метод проверки регистрации с валидными данными
     def registration_with_valid_data(self, first_name, last_name, email_phone, пароль):
         себя. find_element(RegPageLocators. REG_FIRST_NAME_INPUT). send_keys(first_name)
         себя. find_element(RegPageLocators. REG_LAST_NAME_INPUT). send_keys(last_name)
@@ -40,7 +40,7 @@
         себя. find_element(RegPageLocators. REG_ENTER_BUTTON). щелчок()
         Самоутвердиться. is_element_present(EmailConfirmPageLocators. EMAIL_CONF_HEADING), "элемент не найден"
 
-    # RT026 метод проверки ссылки под кнопкой "Зарегистрироваться" на страницу с пользовательским соглашением
+    # TRT021 метод проверки ссылки под кнопкой "Зарегистрироваться" на страницу с пользовательским соглашением
     def link_to_the_user_agreement_page(self):
         original_window = себя. браузер. current_window_handle
         Assert len(self. браузер. window_handles) == 1
@@ -54,7 +54,7 @@
         утверждать «https://b2c.passport.rt.ru/sso-static/agreement/agreement.html» в себе. браузер. current_url, \
             «URL-адрес не совпадает»
 
-    # RT027 метод проверки ссылки в футере на страницу с пользовательским соглашением
+    # TRT022 метод проверки ссылки в футере на страницу с пользовательским соглашением
     def link_fut_to_the_user_agreement_page(self):
         original_window = себя. браузер. current_window_handle
         Assert len(self. браузер. window_handles) == 1
@@ -68,32 +68,32 @@
         утверждать «https://b2c.passport.rt.ru/sso-static/agreement/agreement.html» в себе. браузер. current_url, \
             «URL-адрес не совпадает»
 
-    # RT028 метод проверки валидации текстового поля (ввод невалидных данных)
+    # TRT023 метод проверки валидации текстового поля (ввод невалидных данных)
     def text_field_validation_invalid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_FIRST_NAME_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
         Самоутвердиться. is_element_present(RegPageLocators. REG_ERROR_FIRST_NAME_INPUT), "элемент не найден"
 
-    # RT029 метод проверки валидации поля ввода email или мобильного телефона (ввод невалидных данных)
+    # TRT024 метод проверки валидации поля ввода email или мобильного телефона (ввод невалидных данных)
     def email_or_phone_field_validation_invalid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_EMAIL_PHONE_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
         Самоутвердиться. is_element_present(RegPageLocators. REG_ERROR_INVALID_EMAIL_OR_PHONE_INPUT), "элемент не найден"
 
-    # RT030 метод проверки валидации поля ввода пароля (ввод невалидных данных)
+    # TRT025 метод проверки валидации поля ввода пароля (ввод невалидных данных)
     def password_field_validation_invalid_data(self, input_text):
         себя. find_element(RegPageLocators. REG_PASSWORD_INPUT). send_keys(input_text)
         себя. find_element(BaseLocators. ТЕЛО). щелчок()
         Самоутвердиться. is_element_present(RegPageLocators. REG_ERROR_INVALID_PASSWORD_INPUT), "элемент не найден"
 
-    # RT031 метод проверки заполнения поля подтверждения пароля данными, отличными от введенных в поле ввода пароля
+    # TRT026 метод проверки заполнения поля подтверждения пароля данными, отличными от введенных в поле ввода пароля
     def entering_data_in_the_password_confirmation_field(self, password1, password2): 
         себя. find_element(RegPageLocators. REG_PASSWORD_INPUT). send_keys(пароль1)
         себя. find_element(RegPageLocators. REG_PASSWORD_CONFIRM_INPUT). send_keys(пароль2)
         себя. find_element(RegPageLocators. REG_ENTER_BUTTON). щелчок()
         Самоутвердиться. is_element_present(RegPageLocators. REG_ERROR_PASSWORD_DONT_MATCH), "элемент не найден"
 
-    # RT032 метод проверки регистрации с невалидными данными
+    # TRT027 метод проверки регистрации с невалидными данными
     def registration_with_invalid_data(self, first_name, last_name, email_phone, пароль):
         себя. find_element(RegPageLocators. REG_FIRST_NAME_INPUT). send_keys(first_name)
         себя. find_element(RegPageLocators. REG_LAST_NAME_INPUT). send_keys(last_name)
