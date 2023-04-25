b5994fc628a67f498d0512b70e0d5a403cce39ed
BogdanKrik <117049510+BogdanKrik@users.noreply.github.com>
2023-04-25 16:44:05 +0300

    Update auth_page.py

--- .pages/auth_page.py
+++ .pages/auth_page.py
@@ -1,67 +1,67 @@
-Из valid_email импорта настроек  
-от . base_page импорт BasePage
-от . локаторы импортируют BaseLocators, AuthPageLocators, ChangePassPageLocators, RegPageLocators, \
+from settings import valid_email
+from .base_page import BasePage
+from .locators import BaseLocators, AuthPageLocators, ChangePassPageLocators, RegPageLocators, \
     UserAgreementPageLocators
 
 
-класс AuthPage(BasePage):
+class AuthPage(BasePage):
     # TRT001 метод проверки перехода на форму авторизации
     def the_authorization_form_is_open(self):
-        Самоутвердиться. is_element_present(AuthPageLocators. AUTH_HEADING)
-        утверждать «https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth» в себе. браузер. current_url, \
-            «URL-адрес не совпадает»
+        assert self.is_element_present(AuthPageLocators.AUTH_HEADING)
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth" in self.browser.current_url, \
+            "url do not match"
 
 
     # TRT002 метод проверки типа аутентификации по умолчанию
     def default_authentication_type(self):
-        Самоутвердиться. is_element_present(AuthPageLocators. AUTH_USERNAME_INPUT_PLACEHOLDER_TELEPHONE), \
-            "Элемент не найден"
+        assert self.is_element_present(AuthPageLocators.AUTH_USERNAME_INPUT_PLACEHOLDER_TELEPHONE), \
+            "element not found"
 
     # TRT003 метод проверки автоматического изменения типа аутентификации
     def automatic_change_of_authentication_type(self):
-        себя. find_element(AuthPageLocators. AUTH_USERNAME_INPUT). send_keys(valid_email())
-        себя. find_element(BaseLocators. ТЕЛО). щелчок()
-        Самоутвердиться. is_element_present(AuthPageLocators. AUTH_USERNAME_INPUT_ACTIV_EMAIL), "элемент не найден"
+        self.find_element(AuthPageLocators.AUTH_USERNAME_INPUT).send_keys(valid_email())
+        self.find_element(BaseLocators.BODY).click()
+        assert self.is_element_present(AuthPageLocators.AUTH_USERNAME_INPUT_ACTIV_EMAIL), "element not found"
 
     # TRT004 метод проверки ссылки на форму восстановления пароля
     def link_to_the_password_recovery_form(self):
-        себя. find_element(AuthPageLocators. AUTH_FORGOT_PASSWORD_LINK). щелчок()
-        Утверждение "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
-               в себе. браузер. current_url, "URL не совпадает"
-        Самоутвердиться. is_element_present(ChangePassPageLocators. CHANGE_PASS_HEADING), "элемент не найден"
+        self.find_element(AuthPageLocators.AUTH_FORGOT_PASSWORD_LINK).click()
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials" \
+               in self.browser.current_url, "url do not match"
+        assert self.is_element_present(ChangePassPageLocators.CHANGE_PASS_HEADING), "element not found"
 
     # TRT005 метод проверки ссылки на форму регистрации
     def link_to_the_registration_form(self):
-        себя. find_element(AuthPageLocators. AUTH_REGISTER_LINK). щелчок()
-        Самоутвердиться. is_element_present(RegPageLocators. REG_HEADING), "элемент не найден"
-        Утверждайте "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/registration" \
-               в себе. браузер. current_url, "URL не совпадает"
+        self.find_element(AuthPageLocators.AUTH_REGISTER_LINK).click()
+        assert self.is_element_present(RegPageLocators.REG_HEADING), "element not found"
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/registration" \
+               in self.browser.current_url, "url do not match"
 
     # TRT006 метод проверки ссылки под кнопкой "Войти" на страницу с пользовательским соглашением
-    def link_to_the_user_agreement_page(self):
-        original_window = себя. браузер. current_window_handle
-        Assert len(self. браузер. window_handles) == 1
-        себя. find_element(AuthPageLocators. AUTH_USER_AGREEMENT_LINK). щелчок()
-        для window_handle в себе. браузер. window_handles:
-            Если window_handle != original_window:
-                себя. браузер. switch_to. окно(window_handle)
-            Еще:
-                проходить
-        Самоутвердиться. is_element_present(UserAgreementPageLocators. USER_AGREEMENT_HEADING), "элемент не найден"
-        утверждать «https://b2c.passport.rt.ru/sso-static/agreement/agreement.html» в себе. браузер. current_url, \
-            «URL-адрес не совпадает»
+     def link_to_the_user_agreement_page(self):
+        original_window = self.browser.current_window_handle
+        assert len(self.browser.window_handles) == 1
+        self.find_element(AuthPageLocators.AUTH_USER_AGREEMENT_LINK).click()
+        for window_handle in self.browser.window_handles:
+            if window_handle != original_window:
+                self.browser.switch_to.window(window_handle)
+            else:
+                pass
+        assert self.is_element_present(UserAgreementPageLocators.USER_AGREEMENT_HEADING), "element not found"
+        assert "https://b2c.passport.rt.ru/sso-static/agreement/agreement.html" in self.browser.current_url, \
+            "url do not match"
 
     # TRT007 метод проверки ссылки на страницу авторизации с помощью соцсети "ВКонтакте"
     def link_to_social_vk(self):
-        себя. find_element(AuthPageLocators. AUTH_SOCIAL_VK_LINK). щелчок()
-        утверждать «https://oauth.vk.com/authorize» в себе. браузер. current_url, "URL-адрес не совпадает"
+        self.find_element(AuthPageLocators.AUTH_SOCIAL_VK_LINK).click()
+        assert "https://oauth.vk.com/authorize" in self.browser.current_url, "url do not match"
 
     # TRT008 метод проверки авторизации с незаполненными полями
     def authorization_with_blank_fields(self):
-        себя. find_element(AuthPageLocators. AUTH_TAB_PHONE). щелчок()
-        себя. find_element(AuthPageLocators. AUTH_ENTER_BUTTON). щелчок()
-        Самоутвердиться. is_element_present(AuthPageLocators. AUTH_ERROR_ENTER_PHONE_NUMBER), "элемент не найден"
-        утверждать «https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth» в себе. браузер. current_url, \
-            «URL-адрес не совпадает»
+        self.find_element(AuthPageLocators.AUTH_TAB_PHONE).click()
+        self.find_element(AuthPageLocators.AUTH_ENTER_BUTTON).click()
+        assert self.is_element_present(AuthPageLocators.AUTH_ERROR_ENTER_PHONE_NUMBER), "element not found"
+        assert "https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth" in self.browser.current_url, \
+            "url do not match"
 
   
