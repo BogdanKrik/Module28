87381b4a21051f65622b762824391e1ccd2689b8
BogdanKrik <117049510+BogdanKrik@users.noreply.github.com>
2023-04-25 16:44:59 +0300

    Update base_page.py

--- .pages/base_page.py
+++ .pages/base_page.py
@@ -1,44 +1,44 @@
-из селена. Веб-драйвер. Поддержка. wait import WebDriverWait
-из селена. Веб-драйвер. поддержка импорта expected_conditions как EC
-из селена. общий. исключения импортируют  NoSuchElementException, TimeoutException
-от . локаторы импортируют AuthPageLocators
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from .locators import AuthPageLocators
 
 
 # Конструктор класса. Принимает browser — экземпляр webdriver
-класс BasePage():
+class BasePage():
     def __init__(self, browser, url, timeout=5):
-        себя.  браузер = браузер
-        себя.  url = url
+        self.browser = browser
+        self.url = url
         # команда для неявного ожидания со значением по умолчанию в 5c:
-        себя. браузер. implicitly_wait(тайм-аут)
+        self.browser.implicitly_wait(timeout)
 
     # метод find_element ищет один элемент и возвращает его
     def find_element(self, locator, time=10):
-        return WebDriverWait(self. браузер, время). до тех пор, пока(EC. presence_of_element_located(локатор),
-                                                       message=f"Не удается найти элемент по локатору {locator}")
+        return WebDriverWait(self.browser, time).until(EC.presence_of_element_located(locator),
+                                                       message=f"Can't find element by locator {locator}")
 
     # метод open открывает нужную страницу в браузере, используя метод get()
     def open(self):
-        себя. браузер. get(self. URL-адрес)
+        self.browser.get(self.url)
 
     # метод open_reg_page открывает форму регистрации в браузере, используя метод get()
     def open_reg_page(self):
-        себя. браузер. get(self. URL-адрес)
-        себя. find_element(AuthPageLocators. AUTH_REGISTER_LINK). щелчок()
+        self.browser.get(self.url)
+        self.find_element(AuthPageLocators.AUTH_REGISTER_LINK).click()
 
     # метод is_element_present перехватывает исключение
     # будет использоваться для проверки присутствия элемента на странице
     def is_element_present(self, what):
-        Попробуйте:
-            WebDriverWait(self. браузер, 10). до тех пор, пока(EC. presence_of_element_located(( что)))
-        кроме (NoSuchElementException):
+        try:
+            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((what)))
+        except (NoSuchElementException):
             return False
-        вернуть Правда
+        return True
 
     # метод is_not_element_present будет использоваться для проверки отсутствия элемента на странице
     def is_not_element_present(self, what):
-        Попробуйте:
-            WebDriverWait(self. браузер, 10). до тех пор, пока(EC. presence_of_element_located(( что)))
-        кроме (TimeoutException):
-            вернуть Правда
+        try:
+            WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((what)))
+        except (TimeoutException):
+            return True
         return False
